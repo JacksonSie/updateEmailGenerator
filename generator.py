f1ea9e647b67b58da8ef7dadd77481678375a648
jason3e7 <jason3e7@gmail.com>
2015-12-31 00:20:19 +0800

    Add updates

--- generator.py
+++ generator.py
@@ -36,5 +36,25 @@ writeFile(readFile('newline.html'))
 # updates
 writeFile(readFile('updates/updatesBegin.html'))
 
+writeFile(readFile('updates/updatesTitle.html'))
+
+writeFile(readFile('updates/updatesTableBegin.html'))
+
+table = readFile('updates/updatesTable.html')
+cve = readFile('updates/updatesCVE.html')
+cve += readFile('updates/updatesCVE.html')
+cve += readFile('updates/updatesCVE.html')
+
+version = readFile('updates/updatesVersion.html')
+version += readFile('updates/updatesVersion.html')
+
+table = replaceTarget(table, '[CVEs]', cve)
+table = replaceTarget(table, '[Versions]', version)
+
+writeFile(table)
+writeFile(readFile('updates/updatesTable.html'))
+writeFile(readFile('updates/updatesTable.html'))
+writeFile('</tbody></table>')
+
 # end
 writeFile(readFile('end.html'))
