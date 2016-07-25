81bb3ab789042bae6deaf538760fdc1119f8c194
jackson.sie <somewhereininder@hotmail.com>
2016-07-25 19:32:03 +0800

    fix html tag

--- generator.py
+++ generator.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+def htmlencode(string):
+    return escape(string).replace('\n','<br/>')
+
 def writeFile(string) :
     f = open('email.html','a')
     f.write(string)
@@ -17,7 +20,7 @@ def addChineseTag(string) :
     output = ''
     for c in string.decode('utf-8') :
         if ord(c) >= 128 and inFlag == 0 :
-            output += u'<span style="font-size:14.0pt;font-family:標楷體">'
+            output += u'<span style="font-size:14.0pt;font-family:\'Times New Roman\',\xbc\xd0\xb7\xa2\xc5\xe9">'
             inFlag = 1
         if ord(c) < 128 and inFlag == 1 :
             output += u'</span>'
@@ -50,7 +53,7 @@ def main():
     nrows = used.Row + used.Rows.Count
 
     for i in range(2, nrows) :
-        print 'news : ' + str(i - 1)
+        print ('news : ' + str(i - 1))
         writeFile(replaceTarget(readFile('news/newsTitle.html'), '[newsTitle]', addChineseTag(str(news.Cells(i, 1)))))
         writeFile(replaceTarget(readFile('news/newsUrl.html'), '[newsUrl]', str(news.Cells(i, 2))))
         writeFile(replaceTarget(readFile('news/newsContent.html'), '[newsContent]', addChineseTag(str(news.Cells(i, 3)))))
@@ -65,15 +68,16 @@ def main():
 
     title = ''
     for i in range(2, nrows) :
-        print 'tables : ' + str(i - 1)
+        print ('tables : ' + str(i - 1))
         table = readFile('updates/updatesTable.html')
         CVEs = ''
         CVEtemp = readFile('updates/updatesCVE.html')
-        for CVE in str(updates.Cells(i, 2)).split(',') : 
+        for CVE in re.split('\n|,',str(updates.Cells(i, 2))):
+            if (len(CVE) == 0 or type(CVE) == 'NoneType') : continue
             cve = CVE.split('@')
             temp = CVEtemp
-            temp = replaceTarget(temp, '[CVEnumber]', cve[0])
-            temp = replaceTarget(temp, '[CVEurl]', cve[1])
+            temp = replaceTarget(temp, '[CVEnumber]', htmlencode(cve[0]))
+            temp = replaceTarget(temp, '[CVEurl]', htmlencode(cve[1]))
             CVEs += temp
 
         table = replaceTarget(table, '[content]', str(updates.Cells(i, 3)))
@@ -82,9 +86,8 @@ def main():
         Versions = ''
         versionTemp = readFile('updates/updatesVersion.html')
         for version in str(updates.Cells(i, 5)).split(',') : 
-            Versions += replaceTarget(versionTemp, '[version]', version)
+            Versions += replaceTarget(versionTemp, '[version]', htmlencode(version))
         table = replaceTarget(table, '[risk]', str(updates.Cells(i, 6)))
-
         table = replaceTarget(table, '[CVEs]', CVEs)
         table = replaceTarget(table, '[Versions]', Versions)
 
@@ -119,7 +122,7 @@ if __name__ == '__main__':
     sys.setdefaultencoding('utf-8')
     from os.path import abspath
     import re
-    
+    from cgi import escape
     try :
         import win32com.client
     except ImportError:
